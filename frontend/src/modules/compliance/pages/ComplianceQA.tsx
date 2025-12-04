a0b6e379c59ded45581c667a76ba8fffe0f6f199
1106601034 <christopherchan3003@gmail.com>
2025-12-04 19:36:08 +1000

    feature: update from

--- frontend/src/modules/compliance/pages/ComplianceQA.tsx
+++ frontend/src/modules/compliance/pages/ComplianceQA.tsx
@@ -6,10 +6,20 @@ import {
 
 const ComplianceQA = () => {
     return (
-        <Box>
+        <Box
+            component="form"
+            sx={{ '& .MuiTextField-root': { m: 1, width: '25ch' } }}
+            noValidate
+            autoComplete="off"
+        >
             <h1>Hello</h1>
-            <h3>Description</h3>
-            <TextField></TextField>
+            <h5>I will be ready when you are.</h5>
+            <TextField
+                id="outlined-multiline-static"
+                multiline
+                rows={4}
+                defaultValue="Ask anything"
+            />
             <Button>Send</Button>
         </Box>
     )
