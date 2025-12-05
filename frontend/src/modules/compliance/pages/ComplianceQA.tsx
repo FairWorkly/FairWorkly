1705451001c5692ae4ffbcc2f7a372da74d18fbd
1106601034 <christopherchan3003@gmail.com>
2025-12-05 17:51:46 +1000

    feature: add textfield

--- frontend/src/modules/compliance/pages/ComplianceQA.tsx
+++ frontend/src/modules/compliance/pages/ComplianceQA.tsx
@@ -1,6 +1,14 @@
 import React from "react";
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
+import {
+  Box,
+  Paper,
+  Typography,
+  TextField,
+  Button,
+  IconButton,
+  InputAdornment,
+} from "@mui/material";
+import { Add } from "@mui/icons-material";
 
 export const ComplianceQA: React.FC = () => {
   return (
@@ -11,6 +19,27 @@ export const ComplianceQA: React.FC = () => {
       <Typography variant="body1">
         This page will host the AI Q&A interface for compliance questions.
       </Typography>
+      <TextField
+        id="filled-multiline-flexible-icon"
+        placeholder="Ask anything"
+        variant="standard"
+        multiline
+        required
+        fullWidth
+        maxRows={8}
+        slotProps={{
+          input: {
+            startAdornment: (
+              <InputAdornment position="start">
+                <IconButton>
+                  <Add />
+                </IconButton>
+              </InputAdornment>
+            ),
+          },
+        }}
+      />
+      <Button>Ask</Button>
     </Box>
   );
 };
