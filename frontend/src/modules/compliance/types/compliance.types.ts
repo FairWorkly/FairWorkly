4a150a0338be603a4270439b6152f8eef84111f0
Sam Guo <guocx8@gmail.com>
2025-12-06 14:28:22 +1100

    Resolve conflicts

--- frontend/src/modules/compliance/types/compliance.types.ts
+++
@@ -1,7 +0,0 @@
-export type AskComplianceRequest = {
-    question: string;
-    awardCode?: string;
-    audience: "manager" | "employee";
-  };
-
-  export type AskComplianceQuestionResponse = Record<string,unknown>;
