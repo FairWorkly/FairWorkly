0eee72253eb9e26f59d53e089f3e8fdca53248c9
Sam Guo <guocx8@gmail.com>
2026-01-30 13:57:56 +1100

    Organization Settings Command handler added

--- backend/src/FairWorkly.Application/Settings/Commands/UpdateOrganization/UpdateOrganizationCommand.cs
+++ backend/src/FairWorkly.Application/Settings/Commands/UpdateOrganization/UpdateOrganizationCommand.cs
@@ -1,23 +1,11 @@
-// FairWorkly.Application/Settings/Commands/UpdateOrganization/UpdateOrganizationCommand.cs
-
+using FairWorkly.Domain.Common;
 using MediatR;
 
 namespace FairWorkly.Application.Settings.Commands.UpdateOrganization;
 
-/// <summary>
-/// Command to update organization profile
-/// Updates organization data from Settings page
-/// </summary>
-public record UpdateOrganizationCommand : IRequest<OrganizationDto>
+public record UpdateOrganizationCommand : IRequest<Result<OrganizationDto>>
 {
-    /// <summary>
-    /// Organization ID from JWT claims
-    /// Set by the controller after reading JWT
-    /// </summary>
     public Guid OrganizationId { get; init; }
 
-    /// <summary>
-    /// Updated organization data from client
-    /// </summary>
     public UpdateOrganizationRequest Request { get; init; } = null!;
 }
